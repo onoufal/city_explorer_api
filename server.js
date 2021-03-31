e1ce19eab5580cb498bba97fb99ef45a3a7a9d7b
onoufal <osama.h.noufal@gmail.com>
2021-03-31 08:29:51 +0300

    submitting phase3 errors

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(cors());
 
 
 const handleLoc = (req, res) => {
-  const location = require('./data/location.json');
-  const locinfo = new Location(req.query.city, location);
   try {
-    res.send(locinfo);
+    const location = require('./data/location.json');
+    const locinfo = new Location(req.query.city, location);
+    res.status(200).send(locinfo);
   } catch (error) {
-    res.status(404).send(`Oooops! Something went wrong ${error}`);
+    res.status(500).send(`Oooops! Something went wrong ${error}`);
   }
 }
 
@@ -23,15 +23,26 @@ app.get('/location', handleLoc);
 
 app.get('/weather', handleWeather);
 
-function handleWeather(req, res) {
-  const weather = require('./data/weather.json');
-  const weatherData = [];
-  weather.data.forEach(elem => {
-    weatherData.push(new Weather(elem.weather.description, elem.valid_date));
-  });
-  // console.log(weatherData);
-  res.status(200).json(weatherData);
+app.get('*', handleError);
+
+function handleError(req, res) {
+  res.status(404).send({ status: 404, respondText: 'sorry this page does not exist' });
 }
+
+function handleWeather(req, res) {
+  try {
+    const weather = require('./data/weather.json');
+    const weatherData = [];
+    weather.data.forEach(elem => {
+      weatherData.push(new Weather(elem.weather.description, elem.valid_date));
+    });
+    // console.log(weatherData);
+    res.status(200).send(weatherData);
+  } catch (error) {
+    res.status(500).send(`Oooops Error ${error}`);
+  }
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
